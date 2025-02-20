f1990a21926979d7fdd0db65357e78040e843859
Nguyen Phuoc Hung <zakizika1590623847@gmail.com>
2025-02-20 13:46:33 +0700

    fix

--- Frontend/src/features/home/components/templates/ListBusinessHome.jsx
+++ Frontend/src/features/home/components/templates/ListBusinessHome.jsx
@@ -1,17 +1,52 @@
 /* eslint-disable no-unused-vars */
-import { Col, List, Row, Typography } from "antd";
+import { Card, Col, List, Row, Typography } from "antd";
 import useBusiness from "../../../business/hooks/useBusiness";
 import BoxContainer from "../../../../components/atoms/BoxContainer";
 
+import { useNavigate } from "react-router-dom";
+
 const ListBusinessHome = () => {
-  // const { businessData } = useBusiness();
+  const { businessData } = useBusiness();
+  const navigate = useNavigate();
   return (
     <Row>
       <Col span={24}>
         <Typography.Title level={2} style={{ fontWeight: "bold" }}>
           Quán ăn
         </Typography.Title>
-        <BoxContainer></BoxContainer>
+
+        <BoxContainer>
+          <Row gutter={[16, 16]}>
+            {businessData.map((business) => (
+              <Col
+                key={business._id}
+                xs={24}
+                sm={12}
+                md={8}
+                lg={6}
+                xl={4}
+                style={{ padding: "10px" }}
+              >
+                <Card
+                  hoverable
+                  cover={
+                    <img
+                      style={{ width: "100%", height: "150px" }}
+                      alt="business avatar"
+                      src={business.avatar}
+                    />
+                  }
+                  onClick={() => navigate(`/businesses/${business._id}`)}
+                >
+                  <Card.Meta
+                    title={business.business_name}
+                    description={business.location}
+                  />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </BoxContainer>
         {/* <List
           grid={{ gutter: 16, column: 4 }}
           dataSource={businessData}
