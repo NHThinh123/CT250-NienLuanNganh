b27b5f4e1d81ede9fbf3d33f965acc3a212b86fc
Nguyen Phuoc Hung <zakizika1590623847@gmail.com>
2025-02-24 10:58:56 +0700

    fix(menuContext)

--- Frontend/src/features/menu/components/molecules/MenuContext.jsx
+++ Frontend/src/features/menu/components/molecules/MenuContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useRef } from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const MenuContext = createContext();
 
 export const MenuProvider = ({ children }) => {
