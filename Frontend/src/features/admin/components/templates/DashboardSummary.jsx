d61a47cfbecb7ba6b3b22fdc389bf50c59011b97
Thinh <155166003+NHThinh123@users.noreply.github.com>
2025-03-20 13:57:13 +0700

    d

--- Frontend/src/features/admin/components/templates/DashboardSummary.jsx
+++ Frontend/src/features/admin/components/templates/DashboardSummary.jsx
@@ -1,25 +1,34 @@
 import { Card, Statistic, Row, Col } from "antd";
 
-const DashboardSummary = ({ userCount, businessCount, totalRevenue }) => {
+const DashboardSummary = ({
+  userCount,
+  businessCount,
+  totalRevenue,
+  postCount,
+}) => {
   return (
     <Row gutter={[16, 16]}>
       <Col span={6}>
         <Card>
           <Statistic
-            title={<span style={{ fontWeight: "bold" }}>Số Lượng Doanh Nghiệp</span>}
+            title={
+              <span style={{ fontWeight: "bold" }}>Số Lượng Doanh Nghiệp</span>
+            }
             value={businessCount}
             valueStyle={{ color: "#1890ff" }}
-            suffix="Business"
+            suffix="Quán ăn"
           />
         </Card>
       </Col>
       <Col span={6}>
         <Card>
           <Statistic
-            title={<span style={{ fontWeight: "bold" }}>Số Lượng Người Dùng</span>}
+            title={
+              <span style={{ fontWeight: "bold" }}>Số Lượng Người Dùng</span>
+            }
             value={userCount}
             valueStyle={{ color: "#ec407a" }}
-            suffix=" User"
+            suffix="Người dùng"
           />
         </Card>
       </Col>
@@ -27,9 +36,9 @@ const DashboardSummary = ({ userCount, businessCount, totalRevenue }) => {
         <Card>
           <Statistic
             title="Số lượng post"
-            value="3,462"
+            value={postCount}
             valueStyle={{ color: "#52c41a" }}
-            suffix="-2% than yesterday"
+            suffix="Bài viết"
           />
         </Card>
       </Col>
--- Frontend/src/pages/AdminPage.jsx
+++ Frontend/src/pages/AdminPage.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { useAdmin } from "../features/admin/hooks/useAdmin";
 import DashboardSummary from "../features/admin/components/templates/DashboardSummary";
 import { Charts } from "../features/admin/components/templates/Charts";
-
-
+import { usePost } from "../features/post/hooks/usePost";
 
 const AdminPage = () => {
   const {
@@ -17,23 +15,27 @@ const AdminPage = () => {
     isRevenueLoading,
     revenueError,
   } = useAdmin();
+  const { data } = usePost({
+    search: "",
+    limit: 9999,
+  });
+  const postCount = data?.pages[0]?.pagination?.totalPosts;
   // Xử lý giá trị hiển thị cho Statistic
   const userCount = isUsersLoading
     ? "Loading..."
     : usersError
-      ? "Error"
-      : users.length;
+    ? "Error"
+    : users.length;
   const businessCount = isBusinessesLoading
     ? "Loading..."
     : businessesError
-      ? "Error"
-      : businesses.length;
+    ? "Error"
+    : businesses.length;
   const revenue = isRevenueLoading
     ? "Loading..."
     : revenueError
-      ? "Error"
-      : `${totalRevenue.toLocaleString()}`;
-
+    ? "Error"
+    : `${totalRevenue.toLocaleString()}`;
 
   if (isUsersLoading || isBusinessesLoading) {
     return <div>Loading...</div>;
@@ -45,10 +47,9 @@ const AdminPage = () => {
         userCount={userCount}
         businessCount={businessCount}
         totalRevenue={revenue}
+        postCount={postCount}
       />
       <Charts />
-
-
     </div>
   );
 };
